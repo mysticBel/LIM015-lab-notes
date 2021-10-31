04a8a4001740586ce8255fd726c9371e8b253ed2
mysticBel <dnxbel@gmail.com>
2021-10-30 22:39:14 -0500

    importing components in App.js , Login Page works!

--- my-vision-board/src/App.js
+++ my-vision-board/src/App.js
@@ -1,9 +1,13 @@
 import React , { useState, useEffect } from 'react';
+import Home from './components/Home';
+import Login from './components/Login';
 
 function App() {
-  return (
-    <div className="App">Hello World !🦄✨</div>
-  );
+   // creating global user 
+   const [userGlobal, setUserGlobal] = useState(null);
+
+
+  return <>{ userGlobal ? <Home /> : <Login />} </>
 }
 
 export default App;
