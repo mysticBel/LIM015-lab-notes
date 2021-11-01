cae71b2d331982a675a814e8f4f2205010134dba
mysticBel <dnxbel@gmail.com>
2021-11-01 04:42:35 -0500

    dynamic text in buttons(sign in , register), changing states in Login.js

--- my-vision-board/src/components/Login.js
+++ my-vision-board/src/components/Login.js
@@ -7,18 +7,22 @@ const Login = () => {
 
   return (
     <Container fluid>
-      <Stack gap={3} className="col-md-3 mx-auto">
+      <Stack gap={2} className="col-md-3 mx-auto">
         <Image src="https://www.seekpng.com/png/full/17-171385_this-free-icons-png-design-of-cute-elephant.png" />
         <h1 class="text-center">My Vision Board </h1>
         <h3 class="text-center"> Dream, Believe, Plan, Do ! </h3>
-
+          <h2>{isRegistering ? 'Register': 'Login' }  </h2> 
+          <Button variant="secondary" size="md" type="submit">
+          Enter with Google
+        </Button>
+        <h6 class="text-center"> or </h6>
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
+            {/* <Form.Text className="text-muted">
               We'll never share your email with anyone else.
-            </Form.Text>
+            </Form.Text> */}
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -30,13 +34,15 @@ const Login = () => {
           </Form.Group>
           <div className="d-grid ">
             <Button variant="secondary" size="md" type="submit">
-              Login
+              {isRegistering ? 'Register': 'Login' }  
             </Button>
           </div>
         </Form>
-        <Button variant="secondary" size="md" type="submit">
-          Enter with Google
-        </Button>
+       
+        <Button variant="link" size="md" onClick={() => setIsRegistering(!isRegistering)}>  
+          {isRegistering ? 'Do you already have an account? Sign in': 'Are you new? Register here !' } 
+        </Button> 
+       
       </Stack>
     </Container>
   );
