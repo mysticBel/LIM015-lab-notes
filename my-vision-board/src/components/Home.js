e0f892c3abfa2a7a04e41694c3eb5163c82ab7d0
mysticBel <dnxbel@gmail.com>
2021-11-03 12:04:32 -0500

    implementing signOut in Home.js+ Button SignOut

--- my-vision-board/src/components/Home.js
+++ my-vision-board/src/components/Home.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import firebaseApp from '../credentials';
+import { getAuth, signOut } from 'firebase/auth';
+import {  Container, Button } from 'react-bootstrap';
+
+//initializing: 
+const auth = getAuth(firebaseApp);
+
 const Home = () => {
-    return(
-        <div>
-       Home Page works !🦄✨ 
-        </div>
-    )
+    return <Container>
+        <h4> Hello user! </h4>
+        <Button onClick={() => signOut(auth)}>
+          SignOut
+        </Button>
+    </Container>
+    
 }
 
 export default Home;
